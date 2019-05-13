afb818b726e6326f6e3433694b1a8356cd358abf
bcParker <cparker8890@gmail.com>
2019-05-13 18:39:49 -0400

    first commit

---
+++ api.js
@@ -0,0 +1,12 @@
+const baseURL = 'https://pixabay.com/api/';
+const key = '12476087-d2e6b2fee46d9e07beb4e8915';
+let url;
+
+const searchTerm = document.querySelector('.search');
+const submitBtn = document.querySelector('.submit');
+
+function fetchResult(e) {
+	e.preventDefault();
+	url = baseURL + '?key=' + key + '&q=' + searchTerm.value;
+	console.log('URL:', url);
+}
